45dc2e6fc9d427ca021f9a6e6da1026bf021cf7a
Jonathan Moss <nathaniel.coldfyre@gmail.com>
2018-06-15 13:49:26 -0700

    trying to add database services

---
+++ src/service.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+const API_ROOT = "https://maximum-effort-server.herokuapp.com/api";
+
+const responseData = res => res.data;
+
+const requests = {
+  get: url => axios.get(`${API_ROOT}${url}`).then(responseData),
+  post: (url, payload) =>
+    axios.post(`${API_ROOT}${url}`, payload).then(responseData),
+  delete: url => {
+    console.log(`${API_ROOT}${url}`);
+    axios.delete(`${API_ROOT}${url}`).then(responseData);
+  }
+};
+
+const profiles = {
+  all: page => requests.get(`/profiles`),
+  add: payload => requests.post(`/profiles`, payload),
+  delete: id => requests.delete(`/profiles/${id}`)
+};
+
+export default { profiles };
